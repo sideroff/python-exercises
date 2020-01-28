3c1624b0a300bc2c8e19052bc511c69ac7b2dedc
sideroff <ivan.sideroff@gmail.com>
2020-01-28 13:30:06 +0200

    updated: various itertools

--- various/iteratools_.py
+++ various/iteratools_.py
@@ -6,12 +6,12 @@ import itertools
 HAWAII + IDAHO + IOWA's + OHIO = STATES
 
 
-I + LOVE + YOU == DORA
-SEND + MORE == MONEY
+I + LOVE + YOU = DORA
+SEND + MORE = MONEY
 '''
 # cryptarithms
 lines = '''
-AE + BE = CE
+HAWAII + IDAHO + IOWA's + OHIO = STATES
 '''
 
 word_pattern = '[\w\']+'
@@ -23,6 +23,14 @@ gen = (line.upper().replace("'", '') for line in lines.split('\n') if line)
 # ( silently caught ) by the for loop since that's what it expects to receive
 # when the generator is done.
 
+# itertoos.groupby liniarly groups ( aka groups until a number that is not 
+# part of the group appears ) eg. groupby [1,2,3,4,6,5,7] odd returns 
+# 1,
+# 2,
+# 3,
+# 4, 6,
+# 5, 7
+
 
 def get_gen(lines: str):
     split_lines = lines.split('\n')
@@ -33,43 +41,52 @@ def get_gen(lines: str):
 # gen = get_gen(lines)
 
 
+f = open('itertools.txt','w+')
 
+has_found_solution = False
 for line in gen:
+    
+    has_found_solution = False
     words = re.findall(word_pattern, line)
 
     unique_chars = set(''.join(words))
     unique_chars_list = list(unique_chars)
 
-    print('''
+
+    f.write('''
     Unique characters: {}
     len: {}
-    '''.format(unique_chars, len(unique_chars)))
+    {}
+    '''.format(unique_chars, len(unique_chars), line))
 
     assert len(unique_chars) <= 10, 'Too many unique letters'
 
     first_chars = {word[0] for word in words}
 
+
     for perm in itertools.permutations(range(0,10), len(unique_chars)):
+        guess = line.replace('=','==')
+        has_failed = False
         for idx, num in enumerate(perm):
             if num == 0 and unique_chars_list[idx] in first_chars:
-                print('skipping')
+                has_failed = True
                 break
             
-            guess = line.replace('=', '==')
-            for idx, char in enumerate(unique_chars_list):
-                guess = guess.replace(char, str(perm[idx]))
-            
-            try:
-                if(eval(guess)):
-                    pass
-                    # print(guess)
-            except:
-                print('''
-                {}
-                {}
-                '''.format(unique_chars_list, perm))
+            guess = guess.replace(unique_chars_list[idx], str(num))
+        
+        if has_failed: continue
 
-    pass
-    unicode_values_gen = (ord(c) for c in unique_chars)
+        if eval(guess):
+            has_found_solution = True
+            f.write(guess.replace('==','=') + '\n')
 
-    # print(unique_chars)
+if not has_found_solution:
+    f.write('No solution found.\n')
+
+# # Maps char with ascii code to str / char ascii
+# translation_table = {
+#     65: '1',
+#     66: '2',
+#     67: '3'
+# }
+# 'abc'.translate(translation_table)
